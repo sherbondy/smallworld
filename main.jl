ffabccfa973d945822f9d1f0f2c6acda6dfe1046
Ethan Sherbondy <sherbondye@gmail.com>
2014-05-24 18:50:31 -0400

    brain masking implemented.

--- main.jl
+++ main.jl
@@ -1,22 +1,43 @@
 using NIfTI
 using ImageView, Images
+using Gadfly
 
 function midpoint(val)
     convert(Int, val/2 + 1)
 end
 
-T1_ni = niread("samples/NC_03_T1.nii");
-T1_data = T1_ni.raw
-T1_data -= minimum(T1_data)
-T1_data /= maximum(T1_data)
-# size(ni)
-T1_size = size(T1_data)
-center_slice_1 = squeeze(T1_data[get_midpoint(T1_size[1]),:,:], 1);
-center_slice_2 = squeeze(T1_data[:,get_midpoint(T1_size[2]),:], 2);
-center_slice_3 = squeeze(T1_data[:,:,get_midpoint(T1_size[3])], 3);
+function normalize_ni(ni_data)
+    raw = ni_data.raw
+    raw_norm = raw - minimum(raw)
+    raw_norm /= maximum(raw_norm)
+    raw_norm
+end
 
-c = canvasgrid(2,2);
-ops = [:pixelspacing => [1,1]]
-display(c[1,1], center_slice_1; ops...)
-display(c[1,21], center_slice_2; ops...)
-display(c[2,1], center_slice_3; ops...)
+function display_brain_centers(ni_data)
+    ni_size = size(ni_data)
+    center_slice_1 = squeeze(ni_data[midpoint(ni_size[1]),:,:], 1);
+    center_slice_2 = squeeze(ni_data[:,midpoint(ni_size[2]),:], 2);
+    center_slice_3 = squeeze(ni_data[:,:,midpoint(ni_size[3])], 3);
+
+    c = canvasgrid(2,2);
+    ops = [:pixelspacing => [1,1]]
+    display(c[1,1], center_slice_1; ops...)
+    display(c[1,2], center_slice_2; ops...)
+    display(c[2,1], center_slice_3; ops...)
+end
+
+
+T1_ni = niread("samples/NC_03_T1.nii");
+T1_data = normalize_ni(T1_ni);
+
+# size(ni)
+
+display_brain_centers(T1_data);
+
+mask_ni = niread("samples/NC_03_mask_brain.nii");
+masked_data = normalize_ni(mask_ni);
+
+# Mask T1 to only include the brain
+T1_masked = T1_data;
+T1_masked[masked_data .== 0] = 0;
+display_brain_centers(T1_masked);
